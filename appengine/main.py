262bb290df092084f8678258e89b4c06b340e906
jewishdan18 <jewishdan18@gmail.com>
2011-09-01 21:29:24 -0700

    Crawler ported to App Engine

--- appengine/main.py
+++ appengine/main.py
@@ -1,2 +1,13 @@
-Hi this is Chrsitian 
-Hi, Dan here!
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+import logging
+import cgi
+    
+application = webapp.WSGIApplication(
+  [])
+   
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
