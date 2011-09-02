28ca98245ae1952fc3d37ac01dbb363cd5c4abcf
Christian Johnson <christian@christianjohnson.org>
2011-09-02 14:38:38 -0400

    Web front end

--- appengine/main.py
+++ appengine/main.py
@@ -1,19 +1,30 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from cron.models import Person
 import os
 from google.appengine.ext.webapp import template
 import logging
 import cgi
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        template_values = {}
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+
+class Search(webapp.RequestHandler):
+  def post(self):
+    results = Person.all().filter(first_name=keyword).fetch(100)
+    template_values = {
+      'results': results,
+    }
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
 application = webapp.WSGIApplication(
-  [('/', MainPage),])
+  [('/search', Search),
+   ('/', MainPage),])
    
 def main():
   run_wsgi_app(application)
