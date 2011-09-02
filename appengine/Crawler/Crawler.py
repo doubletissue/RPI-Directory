a2e13d10912f110ae92d92bf416e5938fb94d41d
jewishdan18 <jewishdan18@gmail.com>
2011-09-01 21:31:48 -0700

    Adding Crawler Folder

---
+++ appengine/Crawler/Crawler.py
@@ -0,0 +1,137 @@
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+import logging
+import cgi
+
+class Crawler(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    d = self.parseData(cgi.escape(self.request.get('key')))
+    for k in d.keys():
+      self.response.out.write(k + "-->" + d[k] + '\n')
+    self.response.out.write("-------------------------")
+    
+  #def post(self):
+    #self.response.headers['Content-Type'] = 'text/plain'
+    #self.response.out.write(cgi.escape(self.request.get('key')))
+    #d = self.parseData(cgi.escape(self.request.get('key')))
+    #for k in d.keys():
+      #self.response.out.write(k + "-->" + d[k] + '\n')
+    #self.response.out.write("-------------------------")
+  
+  def getPage( self, key ):
+    url = "http://prod3.server.rpi.edu/peopledirectory/entry.do?datasetName=directory&key=" + str(key)
+    result = urlfetch.fetch(url)
+    
+    if result.status_code != 200:
+      logging.error("Failed to fetch URL: " + url + "; Error code: " + str(result.status_code))
+      return ""
+      
+    return result.content
+    
+  def findAttribute(self,string, attribute):
+    
+    startIndex = string.find(attribute)
+    endIndex   = string.find('</td>',startIndex) + len('</td>')
+    
+    if startIndex < 0:
+      return ""
+    if string[startIndex:endIndex].find("<td />") > 0:
+      return ""
+    else:
+      startIndex = string.find('<td>',startIndex)
+      startIndex = string.find('>',startIndex) + 1
+      endIndex   = string.find('</td>',startIndex)
+      r = string[startIndex:endIndex].strip()
+      r = r.replace("<br />","\n")
+      return r
+      
+  def findName(self,string):
+    startIndex = string.find('<th class="name">')
+    startIndex = string.find('>',startIndex) + 1
+    endIndex   = string.find('</th>',startIndex)
+    return string[startIndex:endIndex].strip()
+
+  def findHomepage(self,string):
+    homepage = self.findAttribute(string,"Homepage:")
+    if homepage is "":
+      return ""
+    startIndex = homepage.find('"') + 1
+    endIndex   = homepage.find('"',startIndex)
+    return homepage[startIndex:endIndex].strip()
+    
+    
+  def findEmail(self,string):
+    email = ""
+    startIndex = 0
+    imagePath = "http://www.rpi.edu/dept/cct/apps/csvdata/directory/resources/glyphs/"
+    while True:
+      if string.find(imagePath,startIndex) < 0:
+        break
+      startIndex = string.find(imagePath, startIndex) + len(imagePath)
+      endIndex   = string.find(".gif", startIndex)
+      
+      letterID = int(string[startIndex:endIndex].strip())
+      if letterID == 99:
+        email += '@rpi.edu'
+      elif letterID < 28:
+        email += chr(letterID+96)
+      else:
+        email += str(letterID-28)
+    return email
+    
+  def findStuff( self,string ):
+    
+    d = {}
+    
+    attributes = {
+                    'Class:'           : 'class',
+                    'Curriculum:'      : 'major',
+                    'Title:'           : 'title',
+                    'Telephone:'       : 'phone',
+                    'Fax:'             : 'fax',
+                    'Office Location:' : 'office_location',
+                    'Campus Mailstop:' : 'campus_mailstop',
+                    'Mailing Address:' : 'mailing_address'
+                  }
+    
+    string = string[string.find('id="singleDirectoryEntry"'):]
+    string = string[:string.find('</table')]
+    
+    d['name'] = self.findName(string)
+    
+    homepage = self.findHomepage(string)
+    if homepage is not "":
+      d['homepage'] = self.findHomepage(string)
+    
+    email = self.findEmail(string)
+    if email is not "":
+      d['email'] = email
+    
+    for k in attributes.keys():
+      v = self.findAttribute(string,k)
+      if v is not "":
+        d[attributes[k]] = v
+    
+    #for k in d.keys():
+      #print k,"-->",d[k]
+    #print "-------------------------"
+    return d
+    
+    
+
+  def parseData(self,key):
+    s = self.getPage(key)
+    return self.findStuff(s)
+
+application = webapp.WSGIApplication(
+  [
+    ("/crawl/.*", Crawler)
+  ])
+   
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
