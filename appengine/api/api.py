51847bc1488291bee5473229f2af3da7e3ffe01c
jewishdan18 <jewishdan18@gmail.com>
2011-09-02 17:08:05 -0700

    table api added

--- appengine/api/api.py
+++ appengine/api/api.py
@@ -45,11 +45,59 @@ class Api(webapp.RequestHandler):
       l.append(Person.buildMap(p))
     s = json.dumps(l)
     self.response.out.write(s)
+
+class ChristiansStupidApi(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    year  = urllib.unquote( cgi.escape(self.request.get('year' )) )
+    major = urllib.unquote( cgi.escape(self.request.get('major')) )
+    name  = urllib.unquote( cgi.escape(self.request.get('name' )) )
+    
+    query = Person.all()
+    
+    if year is not "":
+      query = query.filter('year = ',year)
+    if major is not "":
+      query = query.filter('major = ',major)
+    
+    names = name.split()[:3]
+    if len(names) > 0:
+      query = query.filter('first_name = ', names[0])
+    if len(names) > 1:
+      query = query.filter('last_name = ', names[-1])
+    if len(names) > 2:
+      query = query.filter('middle_name = ', names[1])
+    
+    people = query.fetch(100)
+    
+    if len(names) == 1:
+      query = Person.all()
+      if year is not "":
+        query = query.filter('year = ',year)
+      if major is not "":
+        query = query.filter('major = ',major)
+      query = query.filter('last_name = ', names[0])
+      people.extend(query.fetch(100))
+    
+    l = []
+    
+    for p in people:
+      l.append(Person.buildMap(p))
+    
+    d2 = {}
+    d2['iTotalRecords'] = len(l)
+    d2['iTotalDisplayRecords'] = len(l)
+    d2['sEcho'] = urllib.unquote( cgi.escape(self.request.get('sEcho' )) )
+    d2['aaData'] = l
+    
+    s = json.dumps(d2)
+    self.response.out.write(s)
     
 
 application = webapp.WSGIApplication(
   [
-    ("/api", Api)
+    ("/api", Api),
+    ("/tableApi", ChristiansStupidApi)
   ])
    
 def main():
