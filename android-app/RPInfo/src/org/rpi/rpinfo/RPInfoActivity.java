375cb3d4beb3f71a07871d89b899d5102d08d7be
Michael <michorowitz@gmail.com>
2011-09-11 13:42:23 -0400

    Added a listview for displaying results

--- android-app/RPInfo/src/org/rpi/rpinfo/RPInfoActivity.java
+++ android-app/RPInfo/src/org/rpi/rpinfo/RPInfoActivity.java
@@ -2,7 +2,6 @@ package org.rpi.rpinfo;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,10 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class RPInfoActivity extends Activity {
-	/*
-	 * rpidirectory.appspot.com/api?name=[name]
-	 */
-
 	/** Called when the activity is first created */
 	public void onCreate(Bundle savedInstanceState) {		
         super.onCreate(savedInstanceState);
