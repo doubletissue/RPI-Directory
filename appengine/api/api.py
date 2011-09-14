b23af30f43e2f38b11b7010bb8d7b942ee7767b8
jewishdan18 <jewishdan18@gmail.com>
2011-09-13 22:07:49 -0700

    Improved prefix matching

--- appengine/api/api.py
+++ appengine/api/api.py
@@ -9,6 +9,9 @@ from django.utils import simplejson as json
 class Api(webapp.RequestHandler):
   
   def nameSearch(self,name_type,name,year,major, num_results, page_offset):
+    if name == '':
+      return [], True
+    
     query = Person.all()
     
     if year is not "":
@@ -18,9 +21,16 @@ class Api(webapp.RequestHandler):
     
     query = query.filter(name_type + ' >= ', name)
     query = query.filter(name_type + ' <= ', name+u'\ufffd')
+    
+    query = query.order(name_type)
       
     
-    return query.fetch(num_results)
+    results = query.fetch(num_results)
+    
+    if len(results) > 0:
+      return results, True
+    results,_ = self.nameSearch(name_type,name[:-1],year,major,num_results,page_offset)
+    return results, False
     
   def get(self):
     self.response.headers['Content-Type'] = 'text/plain'
@@ -33,20 +43,20 @@ class Api(webapp.RequestHandler):
     l = []
     
     if len(names) == 1:
-      first_name_results = self.nameSearch('first_name',names[0],year,major, 10, 0)
+      first_name_results, modded = self.nameSearch('first_name',names[0],year,major, 10, 0)
       for p in first_name_results:
         l.append(Person.buildMap(p))
-      last_name_results  = self.nameSearch('last_name',names[0],year,major, 10, 0)
+      last_name_results, modded  = self.nameSearch('last_name',names[0],year,major, 10, 0)
       for p in last_name_results:
         l.append(Person.buildMap(p))
     elif len(names) > 1:
       d = set()
-      first_name_results = self.nameSearch('first_name',names[0],year,major, 1000, 0)
-      for p in first_name_results:
-        d.add(p.key().name())
-      last_name_results  = self.nameSearch('last_name',names[-1],year,major, 1000, 0)
-      i = 0
+      last_name_results, modded = self.nameSearch('last_name',names[-1],year,major, 1000, 0)
       for p in last_name_results:
+        d.add(p.key().name())
+      first_name_results, modded  = self.nameSearch('first_name',names[0],year,major, 1000, 0)
+      i = 0
+      for p in first_name_results:
         if p.key().name() in d:
           l.append(Person.buildMap(p))
           i += 1
