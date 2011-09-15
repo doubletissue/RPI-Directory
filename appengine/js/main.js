56fabaa74299056246035a9c08ca1d9a7dccf1a4
Christian Johnson <christian@christianjohnson.org>
2011-09-15 19:11:51 -0400

    Added delay to JS to query server

--- appengine/js/main.js
+++ appengine/js/main.js
@@ -1,73 +1,85 @@
 // Christian Johnson
 // RCOS RPI Directory JavaScript
 
-var last_known_query = "";
 var keyword = "";
 var delay = 60;
 var padding = '20%';
 
 function parseData(data){  
-	if (data !== [] && data.length > 0){		
-	 $("#results").find("tbody").empty();	
-	 // Loop through JSON
-	 $.each(data, function(i, person){
-	   var table_row = "<tr>";
-       // Loop through each person and output their attributes
-       //$.each(person, function(key, value){
-		 //if (key in {'name':'', 'major':'','class':''}/* && value != undefined*/){
-		 //  table_row += ("<td>" + value + "</td>");	
-		 //}
-	   //});
-	   table_row += ("<td>"+person.name+"</td><td>"+ (person.major == undefined ? 'N/A' : person.major) +"</td><td>"+ (person.class == undefined ? 'N/A' : person.class) +"</td>");
-	   table_row += "</tr>"
-       $("#results").find("tbody").append(table_row);
-	 });
-	 $("#output").empty();
-	 last_known_query = keyword;
-	$("#results").trigger("update");
-	}else if (last_known_query != ''){
-	  $("#output").text("Nothing found for " + keyword + ", showing results for " + last_known_query);
-	}	
+	if (data !== [] && data.length > 0){
+	  // Get rid of current results		
+	  $("#results").find("tbody").empty();
+	 
+	  // Loop through JSON
+	  $.each(data, function(i, person){
+	    var table_row = "<tr>";
+      // Loop through each person and output their attributes
+      //$.each(person, function(key, value){
+		   //if (key in {'name':'', 'major':'','class':''}/* && value != undefined*/){
+		   //  table_row += ("<td>" + value + "</td>");	
+		   //}
+	    //});
+	    table_row += ("<td>"+person.name+"</td><td>"+ (person.major == undefined ? 'N/A' : person.major) +"</td><td>"+ (person.class == undefined ? 'N/A' : person.class) +"</td>");
+	    table_row += "</tr>"
+      $("#results").find("tbody").append(table_row);
+	  });
+
+	  $("#results").trigger("update");
+	  }	
+}
+
+//Function to animate text box:
+// Send true to animate it up, false to animate it down
+function animate(flag){
+  if (flag){
+    $("#container").animate({
+      marginTop: '0%',
+    }, delay, function(){ $("#container").css("margin-top","0%"); });
+  }else{
+    $("#container").animate({
+      marginTop: padding,
+    }, delay * 2);
+  }
 }
 
 $(document).ready(function() {
-	$("#keyword").keyup(function(event) {
-	  var keyword = $("#keyword").val();
-	  var margin = $("#container").css("margin-top");
+	$("#keyword").bindWithDelay("keyup", function(event) {
+	    
+	    $("#results").css("filter", "alpha(opacity=40)");
+	    
+	    var keyword = $("#keyword").val();
+  	  var margin = $("#container").css("margin-top");
 	
-	  // Check for enter keypress
-	  if (event.which == 13) {
-	     event.preventDefault();
-	     return;
-	  }
-	  
-	  if (keyword != ''){
-	    //Animate text box up
-   	  if ( margin != "0%" || margin != "0px" ){
-   	    $("#container").animate({
-   	      marginTop: '0%',
-   	    }, delay, function(){ $("#container").css("margin-top","0%"); });
-   	  }
+  	  // Check for enter keypress
+  	  if (event.which == 13) {
+  	     event.preventDefault();
+  	     return;
+  	  }
+	    
+	    // If a non-blank entry
+  	  if (keyword != ''){
+  	    //Animate text box up
+     	  if ( margin != "0%" || margin != "0px" ){
+     	    animate(true);
+     	  }
    	  
-		  $.ajax({
-		     type: "GET",
-		     url: "/api?name=" + encodeURI(keyword),
-		     async: true,
-		 	 dataType: "json",
-			 success: parseData,
-		   });
+  		  $.ajax({
+		      type: "GET",
+		      url: "/api?name=" + encodeURI(keyword),
+		      async: true,
+		 	    dataType: "json",
+			    success: parseData,
+		    });
 		   
-		  $("#results").show();
-	  }else if (keyword == ''){ // Entry is blank
-	    $("#results").hide();
-	    // $("#output").text("Type something above!");
-	    if ( margin == "0%" || margin == "0px"){
-  		  $("#container").animate({
-		      marginTop: padding,
-		    }, delay*2);
+  		  $("#results").show();
+  	  }else if (keyword == ''){ // Entry is blank
+  	    $("#results").hide();
+  	    // Animate box back down
+  	    //if ( margin == "0%" || margin == "0px"){
+    		//  animate(false);
+  	    //}
 	    }
-	  }
-  });
+	  }, 175);
   
   //Make table sortable
   $("#results").tablesorter();
