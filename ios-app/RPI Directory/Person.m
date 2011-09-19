87f2b41c9f16bf6cd28882152683d759397d0b27
Brendon Justin <brendonjustin@gmail.com>
2011-09-19 10:11:49 -0400

    Copy in iOS application and add .gitignore.

---
+++ ios-app/RPI Directory/Person.m
@@ -0,0 +1,31 @@
+//
+//  Person.m
+//  RPI Directory
+//
+//  Created by Brendon Justin on 9/17/11.
+//  Copyright 2011 Naga Softworks, LLC. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@synthesize name;
+@synthesize major;
+@synthesize rcsid;
+@synthesize email;
+@synthesize homepage;
+@synthesize class;
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
