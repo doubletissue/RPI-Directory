f16ab652e02cc54c2c915c06c92bb9022d005c4f
Michael <michorowitz@gmail.com>
2011-09-20 22:03:16 -0400

    Added fancier detailed view

--- android-app/RPInfo/src/org/rpi/rpinfo/DetailedDataDisplayerActivity.java
+++ android-app/RPInfo/src/org/rpi/rpinfo/DetailedDataDisplayerActivity.java
@@ -1,12 +1,15 @@
 package org.rpi.rpinfo;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -26,14 +29,23 @@ public class DetailedDataDisplayerActivity extends Activity {
 		if( selectedPerson == null ){
 			finish();
 		}
-		
-		Map<String, String> data = selectedPerson.getAllElements();
+				
+		//Extract the data from the selected person and prepare to put it into a nice list view
+		Map<String, String> raw_data = selectedPerson.getAllElements();
+		ArrayList<String> parsed_data = new ArrayList<String>();
+		for( Entry<String, String> entry : raw_data.entrySet() ){
+			if( !entry.getKey().equals("Name") ){
+				parsed_data.add( entry.getKey() + ": " + entry.getValue() );
+			}
+		}
 		
 		//Put the person's name in
 		TextView name = (TextView)this.findViewById(R.id.person_name);
 		name.setText(selectedPerson.getElement("name", "N/A"));
 		
+		//Set up the list view
 		ListView lv = (ListView)this.findViewById(R.id.data_list);
+		lv.setAdapter(new ArrayAdapter<String>(this, R.layout.detailed_view_list_item, parsed_data));
 	}
 
 }
