b5cf3125a514c1116a02d19b7385b1dd76e7048f
Michael <michorowitz@gmail.com>
2011-09-23 09:25:57 -0400

    Made the text output for detailed view all fancy

--- android-app/RPInfo/src/org/rpi/rpinfo/DetailedDataDisplayerActivity.java
+++ android-app/RPInfo/src/org/rpi/rpinfo/DetailedDataDisplayerActivity.java
@@ -8,12 +8,42 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class DetailedDataDisplayerActivity extends Activity {
+	/*
+	 * Format the field name to display correctly
+	 */
+	private String formatField( String field ){
+		String rv = "";
+		
+		//If empty string, can't do much
+		if( field.length() == 0 ){
+			return field;
+		}
+		
+		//The return value should contain everything after the first character
+		if( field.length() > 1 ){
+			rv = field.substring(1);
+		}
+				
+		rv = Character.toUpperCase(field.charAt(0)) + rv;
+		
+		return rv;
+	}
+	
+	/*
+	 * Format the value to display correctly
+	 */
+	private String formatValue( String field, String value ){
+		return value;
+	}
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
@@ -32,10 +62,12 @@ public class DetailedDataDisplayerActivity extends Activity {
 				
 		//Extract the data from the selected person and prepare to put it into a nice list view
 		Map<String, String> raw_data = selectedPerson.getAllElements();
-		ArrayList<String> parsed_data = new ArrayList<String>();
+		//Spanned is like a string but it can contain formatting data and such
+		ArrayList<Spanned> parsed_data = new ArrayList<Spanned>();
 		for( Entry<String, String> entry : raw_data.entrySet() ){
-			if( !entry.getKey().equals("Name") ){
-				parsed_data.add( entry.getKey() + ": " + entry.getValue() );
+			if( !entry.getKey().equals("name") ){
+				//Make the name of the field bold
+				parsed_data.add( Html.fromHtml("<b>" + formatField( entry.getKey() ) + "</b>: " + formatValue( entry.getKey(), entry.getValue() ) ) );
 			}
 		}
 		
@@ -45,7 +77,7 @@ public class DetailedDataDisplayerActivity extends Activity {
 		
 		//Set up the list view
 		ListView lv = (ListView)this.findViewById(R.id.data_list);
-		lv.setAdapter(new ArrayAdapter<String>(this, R.layout.detailed_view_list_item, parsed_data));
+		lv.setAdapter(new ArrayAdapter<Spanned>(this, R.layout.detailed_view_list_item, parsed_data));
 	}
 
 }
