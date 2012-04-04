1530b59bb0040ef3cb02aa51c164df67f67a680b
Michael <michorowitz@gmail.com>
2012-04-04 00:40:26 -0400

    Updated

--- appengine/cron/driver.py
+++ appengine/cron/driver.py
@@ -35,9 +35,10 @@ def putResult(d):
     
   conn = rdbms.connect(instance=_INSTANCE_NAME, database="rpidirectory")
   cursor = conn.cursor()
-  query = 'REPLACE INTO rpidirectory (`name`, `campus_mailstop`, `department`, `email`, `fax`, `first_name`, `homepage`, `last_name`, `mailing_address`, `major`, `office_location`, `phone`, `rcsid`, `title`, `year`) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")' % (person.rcsid, person.campus_mailstop, person.department, person.email, person.fax, person.first_name, person.homepage, person.last_name, person.mailing_address, person.major, person.office_location, person.phone, person.rcsid, person.title, person.year)
-  logging.info(query)
-  cursor.execute(query)
+  query = 'REPLACE INTO rpidirectory (`name`, `campus_mailstop`, `department`, `email`, `fax`, `first_name`, `homepage`, `last_name`, `mailing_address`, `major`, `office_location`, `phone`, `rcsid`, `title`, `year`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+  args = (person.rcsid, person.campus_mailstop, person.department, person.email, person.fax, person.first_name, person.homepage, person.last_name, person.mailing_address, person.major, person.office_location, person.phone, person.rcsid, person.title, person.year)
+  #logging.info(query)
+  cursor.execute(query, args)
   conn.close()
 
 def crawlPerson(index):
