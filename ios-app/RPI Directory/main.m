2817e400b4a6ba89fce8f1d328dab1dcc2420f28
Brendon Justin <brendonjustin@gmail.com>
2012-04-13 15:29:12 -0400

    Change copyright info

--- ios-app/RPI Directory/main.m
+++ ios-app/RPI Directory/main.m
@@ -3,7 +3,7 @@
 //  RPI Directory
 //
 //  Created by Brendon Justin on 4/13/12.
-//  Copyright (c) 2012 Naga Softworks, LLC. All rights reserved.
+//  Copyright (c) 2012 Brendon Justin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
