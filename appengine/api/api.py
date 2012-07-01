40f054b99380dcdf15c02430bf09f75b4703ee25
Christian Johnson <christian@christianjohnson.org>
2012-06-30 23:11:41 -0700

    search api

--- appengine/api/api.py
+++ appengine/api/api.py
@@ -13,6 +13,7 @@ from django.utils import simplejson as json
 from google.appengine.api import search
 
 _INSTANCE_NAME = 'christianjohnson.org:rpidirectory:christianjohnson'
+_INDEX_NAME = 'person'
 
 row_attributes = (['first_name',
                    'last_name',
@@ -28,8 +29,8 @@ row_attributes = (['first_name',
                    'rcsid',
                    'title',
                    'year'])
-                   
-                   
+
+
 query_attributes = (['first_name',
                      'last_name',
                      'campus_mailstop',
@@ -66,7 +67,7 @@ def parse_person_from_sql(raw_row):
     output["name"] = cgi.escape(CamelCase(n))
 
   #Parse the rest
-  for attribute,raw_row_data in zip(row_attributes[2:], raw_row[2:]):
+  for attribute, raw_row_data in zip(row_attributes[2:], raw_row[2:]):
     if raw_row_data not in [None, "None"]:
       if attribute in ["department", "mailing_address", "office_location", "major", "title", "year"]:
         output[attribute] = cgi.escape(CamelCase(raw_row_data))
@@ -84,13 +85,13 @@ def parse_int(i, default):
 class Api(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/plain'
-    search    = str(urllib.unquote(cgi.escape(self.request.get('q')).lower()[:100]))
-    name      = str(urllib.unquote(cgi.escape(self.request.get('name')).lower()[:50]))
-    token     = urllib.unquote(cgi.escape(self.request.get('token')))
-    page_num  = parse_int(urllib.unquote(cgi.escape(self.request.get('page_num'))), 1)
+    search_query = str(urllib.unquote(cgi.escape(self.request.get('q')).lower()[:100]))
+    name = str(urllib.unquote(cgi.escape(self.request.get('name')).lower()[:50]))
+    token = urllib.unquote(cgi.escape(self.request.get('token')))
+    page_num = parse_int(urllib.unquote(cgi.escape(self.request.get('page_num'))), 1)
     page_size = parse_int(urllib.unquote(cgi.escape(self.request.get('page_size'))), 20)
 
-    if search + name == "":
+    if search_query + name == "":
       d = {}
       d['data'] = []
       d['token'] = token
@@ -99,12 +100,12 @@ class Api(webapp.RequestHandler):
       self.response.out.write(s)
       return
 
-    if search == "":
-      search = name
-    
+    if search_query == "":
+      search_query = name
+
     if page_size > 20 or page_size < 1:
       page_size = 20
-    
+
     # Flood Prevention
     ip = str(self.request.remote_addr)
     ipCount = memcache.get(ip)
@@ -113,143 +114,43 @@ class Api(webapp.RequestHandler):
         d = {}
         d['data'] = 'Quota Exceeded'
         d['token'] = token
-        d['q'] = search
+        d['q'] = search_query
         s = json.dumps(d)
         self.response.out.write(s)
 
-        ban_time = 600 + 60 * 2 ** ( (ipCount-1000) )
-        if ban_time > 7*24*60*60:
-          ban_time = 7*24*60*60
+        ban_time = 600 + 60 * 2 ** ((ipCount - 1000))
+        if ban_time > 7 * 24 * 60 * 60:
+          ban_time = 7 * 24 * 60 * 60
         logging.info('Quota exceeded for ' + ip + ', count at ' + str(ipCount) + ', banned for ' + str(ban_time))
-        memcache.replace(ip,ipCount+1,time=ban_time)
+        memcache.replace(ip, ipCount + 1, time=ban_time)
 
         if (ipCount - 1001) % 100 == 0:
           message = mail.EmailMessage(sender="IP Banning <ip-logger@rpidirectory.appspotmail.com>",
                                       subject="RPIDirectory IP " + ip + " Banned")
           message.to = "rpi-directory-ip@googlegroups.com"
-          message.body = "IP: " + ip + "\nban time: " + str(ban_time) + "\nQuery: " + search + "\nHit Count: " + str(ipCount)
+          message.body = "IP: " + ip + "\nban time: " + str(ban_time) + "\nQuery: " + search_query + "\nHit Count: " + str(ipCount)
           message.send()
-          logging.info("EMail sent about ip: " + ip) 
+          logging.info("EMail sent about ip: " + ip)
         return
-      memcache.replace(ip,ipCount+1,time=600)
+      memcache.replace(ip, ipCount + 1, time=600)
     else:
-      memcache.add(ip,1,time=600)
+      memcache.add(ip, 1, time=600)
 
-    queries = map(str, search.split())
-    #Check memcache for results
-    if page_num == 1:
-      memcache_key = ":".join(sorted(search.split()))
-      cached_mem = memcache.get(memcache_key)
-      if cached_mem is not None:
-        d = {}
-        d['data'] = cached_mem
-        d['token'] = token
-        d['q'] = search
-        s = json.dumps(d)
-        self.response.out.write(s)
-        return
-    
-    # If not, we query Cloud SQL
-    conn = rdbms.connect(instance=_INSTANCE_NAME, database='rpidirectory')
-    
-    # If not, we query Cloud SQL
-    cursor = conn.cursor()
+    queries = map(str, search_query.split())
+    results = search.Index(name=_INDEX_NAME).search(search_query.split()[0])
 
-    query = 'SELECT ' + ",".join(row_attributes) + ' from rpidirectory WHERE ' 
-
-    for word in queries:
-      query += '('
-      for field in query_attributes:
-        query += field + ' LIKE ' + "'%" + word + "%'"
-        if field is not query_attributes[-1]:
-          query += ' OR '
-      query += ')'
-      if word is not queries[-1]:
-        query += ' AND '
-        
-    query += ' ORDER BY first_name'
-    query += ' LIMIT ' + str((page_num-1)*page_size) + ',' + str(page_size)
-    
-    #logging.debug(query)
-    try:
-      cursor.execute(query)
-    except DeadlineExceededError:
-      logging.error("Database timeout error")
-      d = {}
-      d['data'] = 'Error with request, please try again'
-      d['token'] = token
-      d['q'] = search
-      s = json.dumps(d)
-      self.response.out.write(s)
-      return None
-    
-    if cursor is None:
-      #Error in DB call
-      return
-    
     d = {}
-    l = []
-    
-    if len(queries) != 0 and cursor.rowcount != 0:
-      for row in cursor.fetchall():
-        l.append(parse_person_from_sql(row))
-    
-    
-    if len(l) > 0:
-      #Log results in MemCache for Stats page
-      #IP address
-      ip = str(self.request.remote_addr)
-      memcache_key = "stats_ip"
-      cached_mem = memcache.get(memcache_key)
-      if cached_mem is not None:
-        cached_mem[ip] += 1
-        memcache.set(memcache_key, cached_mem)
-      else:
-        d = defaultdict(int)
-        d[ip] += 1
-        memcache.set(memcache_key, d)
-      
-      ##Log names searched
-      #if len(names) > 1 and len(names[0]) > 1 and len(names[-1]) > 1:
-        ##First name
-        #memcache_key = "stats_first_names"
-        #cached_mem = memcache.get(memcache_key)
-        #if cached_mem is not None:
-          #cached_mem[names[0].title()] += 1
-          #memcache.set(memcache_key, cached_mem)
-        #else:
-          #d = defaultdict(int)
-          #d[names[0].title()] += 1
-          #memcache.set(memcache_key, d)
-      
-        ##Last name
-        #memcache_key = "stats_last_names"
-        #cached_mem = memcache.get(memcache_key)
-        #if cached_mem is not None:
-          #cached_mem[names[-1].title()] += 1
-          #memcache.set(memcache_key, cached_mem)
-        #else:
-          #d = defaultdict(int)
-          #d[names[-1].title()] += 1
-          #memcache.set(memcache_key, d)
-    
-    d = {}
-    d['data'] = l
+    d['data'] = results.results
     d['token'] = token
-    d['q'] = search
-    
-    #Add to memcache
-    memcache_key = ":".join(sorted(search.split()))
-    memcache.add(memcache_key, l, 518400)
-    logging.debug("Cache miss, adding " + memcache_key + " to MemCache")
-    
-    s = json.dumps(d)
-    self.response.out.write(s)
+    d['q'] = search_query
+    #s = json.dumps(d)
+    self.response.out.write(d)
+
 
 application = webapp.WSGIApplication([
     ("/api", Api)
   ])
-   
+
 def main():
   run_wsgi_app(application)
 
