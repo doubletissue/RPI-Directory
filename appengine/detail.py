66c1b21bed676c502bf1268d4cbd74d71e55508e
Christian Johnson <christian@christianjohnson.org>
2012-09-28 13:21:27 -0400

    JS profile

--- appengine/detail.py
+++ appengine/detail.py
@@ -9,12 +9,15 @@ from models import Person
 
 class DetailPage(webapp2.RequestHandler):
   def get(self, rcs_id):
-    logging.error('RCSID: ' + rcs_id)
     user = users.get_current_user()
+    enabled_user = None
+    if user:
+      enabled_user = Person.query(Person.linked_account == user).get()
     template_values = {"active": "dashboard",
                        "rcs_id": rcs_id,
                        "person": Person.get_by_id(rcs_id),
-                       "user": user}
+                       "user": user,
+                       "enabled_user": enabled_user}
     path = os.path.join(os.path.dirname(__file__), 'html/detail.html')
     self.response.out.write(template.render(path, template_values))
 
