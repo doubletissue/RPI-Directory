14781c89025112d8cd37706b6bf63a0cabc324d1
Michael Horowitz <michorowitz@gmail.com>
2012-11-09 17:27:20 -0500

    Added action bar

--- android-app/RPInfo2/gen/org/rpi/rpinfo/R.java
+++ android-app/RPInfo2/gen/org/rpi/rpinfo/R.java
@@ -13,8 +13,11 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int unknown_person=0x7f020003;
+        public static final int ic_menu_call=0x7f020002;
+        public static final int ic_menu_invite=0x7f020003;
+        public static final int ic_menu_send=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int unknown_person=0x7f020006;
     }
     public static final class id {
         public static final int detail_fragment=0x7f070000;
@@ -22,7 +25,9 @@ public final class R {
         public static final int detail_person_photo=0x7f070006;
         public static final int detail_person_photo_progress=0x7f070007;
         public static final int key=0x7f070008;
-        public static final int menu_settings=0x7f070011;
+        public static final int menu_add_contact=0x7f070013;
+        public static final int menu_dial_phone=0x7f070012;
+        public static final int menu_send_email=0x7f070011;
         public static final int more_button=0x7f070010;
         public static final int person_name=0x7f070002;
         public static final int query_person_photo=0x7f07000b;
@@ -47,10 +52,13 @@ public final class R {
         public static final int view_search_list_more_button=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_search=0x7f060000;
+        public static final int activity_detail=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int menu_item_call=0x7f040005;
+        public static final int menu_item_email=0x7f040004;
+        public static final int menu_item_invite=0x7f040006;
         public static final int menu_settings=0x7f040001;
         public static final int search_term_hint=0x7f040003;
         public static final int title=0x7f040002;
--- android-app/RPInfo2/src/org/rpi/rpinfo/detail/DetailFragment.java
+++ android-app/RPInfo2/src/org/rpi/rpinfo/detail/DetailFragment.java
@@ -5,23 +5,31 @@ import java.util.ArrayList;
 import org.rpi.rpinfo.R;
 import org.rpi.rpinfo.api.PersonModel;
 
+import android.content.ContentProviderOperation;
 import android.content.Context;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.RawContacts;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailFragment extends Fragment {
 
@@ -67,28 +75,67 @@ public class DetailFragment extends Fragment {
         detailListView.setAdapter(new DetailListAdapter(getActivity(),
                 R.layout.view_detail_list_item, models));
 
-        // If a list element is pressed
-        detailListView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                PersonDetail model = (PersonDetail) parent.getAdapter().getItem(position);
-                String key = model.getRawKey();
-                if (key.equals("phone")) {
-                    // Log.i(TAG, "Calling" + model.getValue());
-                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("tel:"
-                            + model.getValue()));
-                    startActivity(intent);
-                } else if (key.equals("email")) {
-                    // Log.i(TAG, "Sending email to " + model.getValue());
-                    Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-                    intent.setType("message/rfc822");
-                    intent.putExtra(android.content.Intent.EXTRA_EMAIL,
-                            new String[] { model.getValue() });
-                    startActivity(Intent.createChooser(intent, "Send email..."));
-                }
-            }
-        });
+        setHasOptionsMenu(true);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.activity_detail, menu);
+
+        final String email = selectedPerson.getElement("email", null);
+        final String phone = selectedPerson.getElement("phone", null);
+
+        MenuItem sendEmail = menu.findItem(R.id.menu_send_email);
+        MenuItem dialPhone = menu.findItem(R.id.menu_dial_phone);
+        MenuItem addContact = menu.findItem(R.id.menu_add_contact);
+
+        if (email == null) {
+            sendEmail.setVisible(false);
+        } else {
+            sendEmail.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                    intent.setType("message/rfc822");
+                    intent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { email });
+                    startActivity(Intent.createChooser(intent, "Send email..."));
+                    return false;
+                }
+            });
+        }
+
+        if (phone == null) {
+            dialPhone.setVisible(false);
+        } else {
+            dialPhone.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("tel:"
+                            + phone));
+                    startActivity(intent);
+                    return false;
+                }
+            });
+        }
+
+        addContact.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                addToContacts();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return true;
     }
 
     private void addPersonImage(final ListView listView, final PersonModel personModel) {
@@ -126,4 +173,94 @@ public class DetailFragment extends Fragment {
 
         listView.addHeaderView(linearLayout, null, false);
     }
+
+    /**
+     * Add the selected person to ones contacts
+     */
+    private boolean addToContacts() {
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+
+        // Backreference value. Don't need to do anything to it.
+        int contact_index = 0;
+
+        // Use the RCS ID as a unique account identifier
+        String accountName = selectedPerson.getElement("rcsid", null);
+        if (accountName != null) {
+            ops.add(ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
+                    .withValue(RawContacts.ACCOUNT_TYPE, null)
+                    .withValue(RawContacts.ACCOUNT_NAME, null).build());
+        } else {
+            // Log.i(TAG, "Person does not have an RCS id. Cannot add to contacts.");
+            return false;
+        }
+
+        String personName = selectedPerson.getElement("name", null);
+        if (personName != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, contact_index)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                            personName).build());
+        } else {
+            // Log.i(TAG, "Person does not have a name.");
+        }
+
+        String personEmail = selectedPerson.getElement("email", null);
+        if (personEmail != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, contact_index)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE,
+                            ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                    .withValue(ContactsContract.CommonDataKinds.Email.DATA1, personEmail).build());
+        } else {
+            // Log.i(TAG, "Person does not have a e-mail address.");
+        }
+
+        String personPhoneNumber = selectedPerson.getElement("phone", null);
+        if (personEmail != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, contact_index)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                            ContactsContract.CommonDataKinds.Phone.TYPE_WORK)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, personPhoneNumber)
+                    .build());
+        } else {
+            // Log.i(TAG, "Person does not have a phone number.");
+        }
+
+        String personWebsite = selectedPerson.getElement("homepage", null);
+        if (personWebsite != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, contact_index)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Website.TYPE,
+                            ContactsContract.CommonDataKinds.Website.TYPE_HOMEPAGE)
+                    .withValue(ContactsContract.CommonDataKinds.Website.URL, personWebsite).build());
+        } else {
+            // Log.i(TAG, "Person does not have a website.");
+        }
+
+        try {
+            getActivity().getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (OperationApplicationException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(getActivity(), "Added to contacts.", Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
 }
