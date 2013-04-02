df103df10e13ccc4edff47e15de274dab8ff69cf
Christian Johnson <christian@christianjohnson.org>
2013-04-02 15:29:06 -0400

    status main page

--- appengine/main.py
+++ appengine/main.py
@@ -3,7 +3,7 @@ from google.appengine.api import users
 import os
 import jinja2
 import logging
-from models import Person
+from models import Person, SearchPosition
 
 from google.appengine.api import search
 
@@ -14,10 +14,8 @@ jinja_environment = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
   def get(self):
     user = users.get_current_user()
-    for index in search.get_indexes(fetch_schema=True):
-      logging.info("index %s", index.name)
-      logging.info("schema: %s", len(index.schema))
-    template_values = {'user': user}
+    per_c = int(SearchPosition.get_by_id("index").position) / 150
+    template_values = {'user': user, 'per_c' : per_c}
     #Site is under maintaince
     #template = jinja_environment.get_template('html/index.html')
     template = jinja_environment.get_template('html/maintenance.html')
